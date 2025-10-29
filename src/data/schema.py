7769bd3b4a667438294b2b9a43ebd2f868e8bb98
JC <cabraljaron03@gmail.com>
2025-10-28 17:10:33 -0700

    fix sqlalchemy 2.0 declarative_base deprecation warning

--- src/data/schema.py
+++ src/data/schema.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from sqlalchemy import Column, Integer, Float, String, DateTime, Boolean, Index
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
